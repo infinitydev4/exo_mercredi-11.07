90f55617d3e2394bc15decd745172025c12b3319
ruby <infinitydev4>
2018-07-11 09:32:51 -0700

    api key

--- bot-twitter.rb
+++ bot-twitter.rb
@@ -3,9 +3,9 @@ require 'twitter'
 Dotenv.load
 # quelques lignes qui enregistrent les clés d'APIs
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV "consumer_key"
-    config.consumer_secret     = ENV "consumer_secret"
-    config.access_token        = ENV "access_token"
-    config.access_token_secret = ENV "access_token_secret"
+    config.consumer_key        = ENV["consumer_key"]
+    config.consumer_secret     = ENV["consumer_secret"]
+    config.access_token        = ENV["access_token"]
+    config.access_token_secret = ENV["access_token_secret"]
   end
 p client
